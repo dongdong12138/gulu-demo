6b00e4496b6b3d62474e0bd14f1d39a8e042c517
BarryDong <286084845@qq.com>
2019-06-23 11:24:22 +0800

    创建 button-group

---
+++ src/button-group.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+  .g-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+
+    > .g-button {
+      border-radius: 0;
+
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+
+      &:first-child {
+        border-top-left-radius: 4px;
+        border-bottom-left-radius: 4px;
+        &:hover {
+          border-color: red;
+        }
+      }
+      &:last-child {
+        border-top-right-radius: 4px;
+        border-bottom-right-radius: 4px;
+        margin-left: -1px;
+        &:hover {
+          border-color: red;
+        }
+      }
+
+    }
+  }
+</style>
