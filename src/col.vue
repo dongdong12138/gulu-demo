fe0873bdb7565fbe7fe9bec7ecfa8b88efd7028f
BarryDong <286084845@qq.com>
2019-06-26 11:13:45 +0800

    row 和 col 的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GuluCol',
+    props: {
+      span: [String, Number]
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  $class-prefix: col-;
+  .col {
+    width: 50%;
+    height: 100px;
+    background: yellowgreen;
+    border: 1px solid red;
+
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+  }
+
+</style>
