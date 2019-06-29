99c46daeb7a53ab362fa82326cd15f66cf40209a
BarryDong <286084845@qq.com>
2019-06-29 09:14:50 +0800

    基本功能完成

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Header from './header'
+import Content from './content'
+import Footer from './footer'
+import Sider from './sider'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -12,6 +17,11 @@ Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-sider', Sider)
 
 var app = new Vue({
   el: '#app',
@@ -22,12 +32,6 @@ var app = new Vue({
     message: 'hi'
   },
   created() {
-    setTimeout(() => {
-      let event = new Event('change')
-      let inputElement = this.$el.querySelector('input')
-      inputElement.dispatchEvent(event)
-      console.log('hi')
-    }, 3000)
   },
   methods: {
     inputChange(e) {
